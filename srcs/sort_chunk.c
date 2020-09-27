ad61021ea93a2d00799779a4eb1c2ba6751b9cfc
Pekka Tuukkanen <petuukka@gmail.com>
2020-09-27 14:15:47 +0300

    new logic to sort chunk from stack a

--- srcs/sort_chunk.c
+++ srcs/sort_chunk.c
@@ -76,6 +76,8 @@ static void	sort_b_n(t_stacks *stacks, t_chunk *chunk, int remaining)
 
 	while (remaining > 0)
 	{
+		if (remaining == 2 && FIRST(stacks->b) > SECOND(stacks->b))
+			swap_b(stacks);
 		position = find_position(stacks->a, FIRST(stacks->b), chunk->size - remaining, chunk);
 		rotate_a(stacks, position);
 		reverse_rotate_a(stacks, position);
@@ -86,6 +88,87 @@ static void	sort_b_n(t_stacks *stacks, t_chunk *chunk, int remaining)
 	}
 	while (in_chunk(FIRST(stacks->a), chunk))
 		rotate_a(stacks, 1);
+}
+
+static int	calculate_moves(t_twlist *lst, t_chunk *chunk, int hold)
+{
+	int	ret1;
+	int	ret2;
+
+	ret1 = 0;
+	ret2 = 0;
+	if (!in_chunk(FIRST(lst), chunk))
+		return (0);
+	if (FIRST(lst) < hold)
+	{
+		return (1 + calculate_moves(lst->next, chunk, hold));
+	}
+	else
+	{
+		ret1 = calculate_moves(lst->next, chunk, hold);
+		ret2 = calculate_moves(lst->next, chunk, FIRST(lst));
+		if (ret2 <= ret1)
+			return (ret2);
+		return (1 + ret1);
+	}
+}
+
+static int	calculate_hold(t_twlist *lst, t_chunk *chunk)
+{
+	int			i;
+	int			selected;
+	int			selected_moves;
+	int			current_moves;
+
+	i = 0;
+	selected = 0;
+	selected_moves = INT32_MAX;
+	current_moves = 0;
+	while (in_chunk(FIRST(lst), chunk))
+	{
+		current_moves = calculate_moves(lst, chunk, FIRST(lst));
+		if (current_moves + i < selected_moves)
+		{
+			selected_moves = current_moves + i;
+			selected = i;
+		}
+		i++;
+		lst = lst->next;
+	}
+	return (selected);
+}
+
+static void	sort_a_n(t_stacks *stacks, t_chunk *chunk)
+{
+	int i;
+	int	hold;
+
+	i = calculate_hold(stacks->a, chunk);
+	while (i > 0)
+	{
+		push_b(stacks);
+		i--;
+	}
+	hold = FIRST(stacks->a);
+	while (in_chunk(FIRST(stacks->a), chunk))
+	{
+		if (FIRST(stacks->a) < hold)
+			push_b(stacks);
+		else
+		{
+			if (calculate_moves(stacks->a->next, chunk, hold) <
+				calculate_moves(stacks->a->next, chunk, FIRST(stacks->a)))
+				push_b(stacks);
+			else
+			{
+				hold = FIRST(stacks->a);
+				rotate_a(stacks, 1);
+			}
+		}
+	}
+	if (!in_chunk(FIRST(stacks->a), chunk) && in_chunk(PREV(stacks->a), chunk))
+		reverse_rotate_a(stacks, -1);
+	sort_b_n(stacks, chunk, ft_twlstcount(stacks->b));
 }
 
 static void	sort_b(t_stacks *stacks, t_chunk *chunk)
@@ -105,9 +188,9 @@ static void	sort_b(t_stacks *stacks, t_chunk *chunk)
 
 static void	sort_a(t_stacks *stacks, t_chunk *chunk)
 {
-	int	i;
+	// int	i;
 
-	i = 0;
+	// i = 0;
 	if (chunk->size == 1)
 		exec_operations(stacks, "ra\n");
 	else if (chunk->size == 2 && FIRST(stacks->a) < SECOND(stacks->a))
@@ -119,14 +202,15 @@ static void	sort_a(t_stacks *stacks, t_chunk *chunk)
 				THIRD(stacks->a));
 	else
 	{
-		while (i + 2 < chunk->size)
-		{
-			push_b(stacks);
-			i++;
-		}
-		if (FIRST(stacks->a) > SECOND(stacks->a))
-			swap_a(stacks);
-		sort_b_n(stacks, chunk, i);
+		return (sort_a_n(stacks, chunk));
+		// while (i + 2 < chunk->size)
+		// {
+		// 	push_b(stacks);
+		// 	i++;
+		// }
+		// if (FIRST(stacks->a) > SECOND(stacks->a))
+		// 	swap_a(stacks);
+		// sort_b_n(stacks, chunk, i);
 	}
 }
 
